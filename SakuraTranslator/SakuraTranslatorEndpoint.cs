124f9eec8040d89c92e12f01a427366a08a21bb9
Tabing010102 <tabing010102@outlook.com>
2024-03-18 02:12:49 +0800

    add MaxConcurrency to config

--- SakuraTranslator/SakuraTranslatorEndpoint.cs
+++ SakuraTranslator/SakuraTranslatorEndpoint.cs
@@ -21,13 +21,14 @@ namespace SakuraTranslator
 
         public string FriendlyName => "Sakura Translator";
 
-        public int MaxConcurrency => 1;
+        public int MaxConcurrency => _maxConcurrency;
 
         public int MaxTranslationsPerRequest => 1;
 
         // params
         private string _endpoint;
         private string _apiType;
+        private int _maxConcurrency;
         private bool _useDict;
         private string _dictMode;
         private Dictionary<string, string> _dict;
@@ -39,6 +40,10 @@ namespace SakuraTranslator
         {
             _endpoint = context.GetOrCreateSetting<string>("Sakura", "Endpoint", "http://127.0.0.1:8080/completion");
             _apiType = context.GetOrCreateSetting<string>("Sakura", "ApiType", string.Empty);
+            if (!int.TryParse(context.GetOrCreateSetting<string>("Sakura", "MaxConcurrency", "1"), out _maxConcurrency))
+            {
+                _maxConcurrency = 1;
+            }
             if (!bool.TryParse(context.GetOrCreateSetting<string>("Sakura", "UseDict", string.Empty), out _useDict))
             {
                 _useDict = false;
