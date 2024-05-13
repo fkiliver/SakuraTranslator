bd4ebea784c0caeb2bfba53d007af759dec692e2
Tabing010102 <tabing010102@outlook.com>
2024-05-14 00:25:49 +0800

    feat: add support for Sakura-32B-v0.10pre1

--- SakuraTranslator/SakuraTranslatorEndpoint.cs
+++ SakuraTranslator/SakuraTranslatorEndpoint.cs
@@ -226,6 +226,10 @@ namespace SakuraTranslator
             {
                 json = MakeOpenAIPrompt(line);
             }
+            else if (_apiType == "Sakura32bV010")
+            {
+                json = MakeSakura32bV010Prompt(line);
+            }
             else
             {
                 json = $"{{\"frequency_penalty\": 0.2, \"n_predict\": 1000, \"prompt\": \"<reserved_106>将下面的日文文本翻译成中文：{line}<reserved_107>\", \"repeat_penalty\": 1, \"temperature\": 0.1, \"top_k\": 40, \"top_p\": 0.3}}";
@@ -296,6 +300,63 @@ namespace SakuraTranslator
                        $"}}" +
                        $"]";
             }
+            return $"{{" +
+                       $"\"model\": \"sukinishiro\"," +
+                       $"\"messages\": " +
+                       messagesStr +
+                       $"," +
+                       $"\"temperature\": 0.1," +
+                       $"\"top_p\": 0.3," +
+                       $"\"max_tokens\": 1000," +
+                       $"\"frequency_penalty\": 0.2," +
+                       $"\"do_sample\": false," +
+                       $"\"top_k\": 40," +
+                       $"\"um_beams\": 1," +
+                       $"\"repetition_penalty\": 1.0" +
+                       $"}}";
+        }
+
+        private string MakeSakura32bV010Prompt(string line)
+        {
+            string messagesStr = string.Empty;
+            var messages = new List<PromptMessage>
+                {
+                    new PromptMessage
+                    {
+                        Role = "system",
+                        Content = "你是一个轻小说翻译模型，可以流畅通顺地使用给定的术语表以日本轻小说的风格将日文翻译成简体中文，并联系上下文正确使用人称代词，注意不要混淆使役态和被动态的主语和宾语，不要擅自添加原文中没有的代词，也不要擅自增加或减少换行。"
+                    }
+                };
+            string dictStr;
+            if (_useDict == false)
+            {
+                dictStr = string.Empty;
+            }
+            else if (_dictMode == "Full")
+            {
+                dictStr = _fullDictStr;
+            }
+            else
+            {
+                var usedDict = _dict.Where(x => line.Contains(x.Key));
+                if (usedDict.Count() > 0)
+                {
+                    var dictStrings = GetDictStringList(usedDict);
+                    dictStr = string.Join("\n", dictStrings.ToArray());
+                }
+                else
+                {
+                    dictStr = string.Empty;
+                }
+            }
+            messages.Add(new PromptMessage
+            {
+                Role = "user",
+                Content = $"根据以下术语表（可以为空）：\n{dictStr}\n\n" +
+                          $"将下面的日文文本根据上述术语表的对应关系和备注翻译成中文：{line}"
+            });
+            messagesStr = SerializePromptMessages(messages);
+
             return $"{{" +
                        $"\"model\": \"sukinishiro\"," +
                        $"\"messages\": " +
